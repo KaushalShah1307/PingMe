6bfa0ca38aef24c673ee5382aea9c79a393a4963
Kaushal Shah <KaushalShah1307@gmail.com>
2016-05-22 04:15:04 -0400

    adding monitor with websites to iterate over and ping after specified intervals

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 /**
  * Created by kshah on 5/22/16.
  */
+var Ping = require('./lib/ping'),
+    websites = [
+        {
+            url: 'http://www.google.com/',
+            timeout: 5
+        },
+        {
+            url: 'http://www.forbes.com/',
+            timeout: 1
+        }
+    ],
+    monitors = [];
+websites.forEach(function (website) {
+    var monitor = new Ping ({
+        website: website.url,
+        timeout: website.timeout
+    });
+    monitors.push(monitor);
+});
